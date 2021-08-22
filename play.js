0383756369d210c77d0ecbeb0a43054dce9f5535
hrakad <your@email.com>
2021-08-22 05:08:17 +0000

    Refactor by adding new file and move the user input functions to it

--- play.js
+++ play.js
@@ -1,25 +1,12 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
 
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (data) => {
-    return handleUserInput(data);
-  })
-  return stdin;
-}
-
 setupInput();
+
+
 
 
 
