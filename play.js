4dba8132e2774604e34aec6e30c992d3e6296426
hrakad <your@email.com>
2021-08-22 04:22:49 +0000

    Setup the standard input and handle the user input

--- play.js
+++ play.js
@@ -1,5 +1,27 @@
 const { connect } = require('./client');
 
-
 console.log("Connecting ...");
 connect();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (data) => {
+    return handleUserInput(data);
+  })
+  return stdin;
+}
+
+setupInput();
+
+
+
+
+
