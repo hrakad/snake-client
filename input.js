86ed55cd7efb71f938ac78473c139fbfc7801b4b
hrakad <your@email.com>
2021-08-23 03:41:41 +0000

    Refactor to send data from input module- include the movement

--- input.js
+++ input.js
@@ -1,9 +1,21 @@
+let connection;
+
 const handleUserInput = function(key) {
-  if (key === '\u0003') {
+  if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left")
+  } else if (key === "s") {
+    connection.write("Move: down")
+  } else if (key === "d") {
+    connection.write("Move: right")
+  } else if (key === '\u0003') {
     process.exit();
   }
 }
-const setupInput = function() {
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
